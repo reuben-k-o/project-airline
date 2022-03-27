1cde1d2c1b748797564ea249ed8586efa8523c7c
reuben-k-o <rubenkhaemba@gmail.com>
2022-03-27 16:08:50 +0300

    updated bookingDetail model

--- models/bookingDetail.js
+++ models/bookingDetail.js
@@ -1,21 +1,20 @@
-const mongoose = require("mongoose"),
-    Schema = mongoose.Schema;
+const { Schema, model } = require("mongoose");
 
-const bookingDetailSchema = new Schema ({
-    from: String,
-    to: String,
-    date: String,
-    airline: String,
-    fromTime: String,
-    toTime: String,
-    user : {
-         type: Schema.Types.ObjectId,
-         ref: "User"
-     },
-     group: String,
-     passengerCount: Number,
-     passengers: Array,
-     price: Number
+const bookingDetailSchema = new Schema({
+  from: String,
+  to: String,
+  date: String,
+  airline: String,
+  fromTime: String,
+  toTime: String,
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  group: String,
+  passengerCount: Number,
+  passengers: Array,
+  price: Number,
 });
 
-module.exports = mongoose.model("BookingDetail", bookingDetailSchema);
+module.exports = model("BookingDetail", bookingDetailSchema);
