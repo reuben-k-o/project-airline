b7cb4b9b32eb48a65d9c791394f17b3b2428fe01
reuben-k-o <rubenkhaemba@gmail.com>
2022-04-18 23:12:52 +0300

    validate email

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,38 +1,41 @@
 const _ = require("lodash");
 const User = require("../models/user");
 
-
 module.exports.renderRegister = (req, res) => {
-    res.render("auth/register");
-}
+  res.render("auth/register");
+};
 
 module.exports.register = async (req, res, next) => {
-    try {
-        let { name, username, password } = req.body;
-        name = _.startCase(_.camelCase(name));
-        const user = new User({ name, username });
-        const registeredUser = await User.register(user, password);
-        req.login(registeredUser, err => {
-            if (err) return next(err);
-            res.redirect("/");
-        });
-    } catch (e) {
-        req.flash("error", e.message);
-        res.redirect("/register");
-    }
-}
+  try {
+    let { name, username, password } = req.body;
+    name = _.startCase(_.camelCase(name));
+    const user = new User({ name, username });
+    const registeredUser = await User.register(user, password);
+    req.login(registeredUser, (err) => {
+      if (err) return next(err);
+      res.redirect("/");
+    });
+  } catch (e) {
+    req.flash("error", e.message);
+    res.redirect("/register");
+  }
+};
 
 module.exports.renderLogin = (req, res) => {
-    res.render("auth/login");
-}
+  res.render("auth/login");
+};
 
 module.exports.login = (req, res) => {
-    const redirectUrl = req.session.returnTo || "/";
-    res.redirect(redirectUrl);
-}
+  const redirectUrl = req.session.returnTo || "/";
+  res.redirect(redirectUrl);
+};
 
 module.exports.logout = (req, res) => {
-    req.logout();
-    req.flash("success", "Goodbye!");
-    res.redirect("/");
-}
+  req.logout();
+  req.flash("success", "Goodbye!");
+  res.redirect("/");
+};
+
+module.exports.signup = (req, res, next) => {
+  const { email, username, password } = req.body;
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,45 @@
 const express = require("express"),
-    router = express.Router(),
-    passport = require("passport");
+  router = express.Router(),
+  passport = require("passport");
+const { check } = require("express-validator");
+const User = require("../models/user");
 
 const catchAsync = require("../utils/catchAsync");
 const auth = require("../controllers/auth");
+router
+  .route("/register")
+  .get(auth.renderRegister)
+  .post(catchAsync(auth.register));
 
-router.route("/register")
-    .get(auth.renderRegister)
-    .post(catchAsync(auth.register));
-
-router.route("/login")
-    .get(auth.renderLogin)
-    .post(passport.authenticate("local", { failureFlash: true, failureRedirect: "/login" }), auth.login);
+router
+  .route("/login")
+  .get(auth.renderLogin)
+  .post(
+    passport.authenticate("local", {
+      failureFlash: true,
+      failureRedirect: "/login",
+    }),
+    auth.login
+  );
 
 router.get("/logout", auth.logout);
 
+router.post(
+  "/signup",
+  [
+    check("email")
+      .isEmail()
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then((userDoc) => {
+          if (userDoc) {
+            return Promise.reject(
+              "E-mail already exists, Kindly choose another one!"
+            );
+          }
+        });
+      }),
+  ],
+  auth.signup
+);
 
 module.exports = router;
